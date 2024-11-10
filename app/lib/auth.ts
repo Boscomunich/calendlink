09c374de42821b7ac44dde466fb82e73ee7b703a
boscomunich <solomon4chidera@gmail.com>
2024-11-10 10:50:30 +0100

    update

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -6,6 +6,5 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-
   providers: [GitHub, Google],
 });
